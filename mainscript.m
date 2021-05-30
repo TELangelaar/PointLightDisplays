2bad87a0330118e17d49ae20b60a860f9403e4c6
TELangelaar <t.e.langelaar@student.rug.nl>
2021-05-30 21:14:03 +0200

    Finalized first draft of the analysis.

--- mainscript.m
+++ mainscript.m
@@ -1,8 +1,19 @@
-function mainscript(do_plot)
+function mainscript(do_plot, do_plot2)
+% MAINSCRIPT This script performs the main computations and transformations
+% for the Point-Light-Displays.
+% MAINSCRIPT(true) plots a random participant for the unmodified data
+% MAINSCRIPT(false, true) plots a random participants for the modified data
+% MAINSCRIPT(true, true) plots a random participant for both the modified
+% and unmodified data (the same participant will be used)
+
 clc;
 close all;
 
+if nargin<2, do_plot2=false; end
 if nargin<1, do_plot=false; end
+
+pp = -1;
+state = -1;
 
 %% Load files into workspace
 % the file 'gaitdata.mat' contains transformed motion data as per the
@@ -45,7 +56,7 @@ if nargin<1, do_plot=false; end
 if exist('gaitdata.mat','file') == 2
     load gaitdata.mat data invalid %we only need the original data and the invalid matrix
 else
-    fprintf("something went wrong with importing gaitdata.mat.\nClosing...\n")
+    error("You have no 'gaitdata.mat'.\nClosing...\n")
     return
 end
 
@@ -75,9 +86,166 @@ n_markers = size(data,3);
 n_dimensions = size(data,4);
 n_time = size(data,5);
 
+hip_R_marker = 3;    %marker locations come from the Gait.dbbuild.get_conf function
+hip_L_marker = 6;
+
+shoulder_R_marker = 11;
+shoulder_L_marker = 14;
+
+padding = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"; %for printing purposes
+
 %% Backangle calculation
 % find the vector that points from the middle of the two outer hip markers
 % to the middle of the two shoulder markers (Y & Z)
+[backangle, backvector, hip_middle_calculated, shoulder_middle_calculated] = calculate_backangle_backvector(data);
+
+if do_plot && ~do_plot2
+%For inspection purposes; plot a random participant and state in blue, with
+%the calculated markers and resulting vector in red. Also display the back
+%angle
+    pp = randi(n_participants);
+    state = randi(n_states);
+
+    plot_inspection(squeeze(data(pp, state, :, :, :)), ...
+        squeeze(hip_middle_calculated(pp, state, :, :)), ...
+        squeeze(shoulder_middle_calculated(pp, state, :, :)), ...
+        squeeze(backangle(pp, state, :)));
+
+end
+
+%% Static Translation
+% translation of the shoulder and outer hip markers to reduce the trunk
+% angle
+
+% Background: 
+%   - Aminiaghdam, Soran; Blickhan, Reinhard; Muller, Roy; Rode, Christian (2017): Posture alteration as a measure to accommodate uneven ground in able-bodied gait. figshare. Dataset. https://doi.org/10.6084/m9.figshare.5692006 
+%   - Titus, A. W., Hillier, S., Louw, Q. A., & Inglis-Jassiem, G. (2018). An analysis of trunk kinematics and gait parameters in people with stroke. African journal of disability, 7, 310. https://doi.org/10.4102/ajod.v7i0.310
+% Average backangle seems to be around 4 - 6 degrees during normal walking
+
+% step 1: calculate the average backangle per participant/state
+mean_back_angle = mean(backangle,3);
+
+mean_angle_per_state = mean(mean_back_angle,1);
+minimal_angle_per_state = min(mean_back_angle,[],1);
+maximal_angle_per_state = max(mean_back_angle,[],1);
+
+states = {'Neutral','Happy','Sad','Angry'};
+disp(padding);
+txt1 = sprintf("Backangle (degrees):\n\t\t\tMin\t\tMean\tMax\n");
+for i = 1:n_states
+    if strcmp(states{i},'Sad') %extra whitespace for alignment purposes
+        txt1 = txt1+sprintf("-%s: \t\t%.2f\t%.2f\t%.2f\n", states{i}, ...
+           minimal_angle_per_state(i),...
+           mean_angle_per_state(i),...
+           maximal_angle_per_state(i));
+    else
+       txt1 = txt1+sprintf("-%s: \t%.2f\t%.2f\t%.2f\n", states{i}, ...
+           minimal_angle_per_state(i),...
+           mean_angle_per_state(i),...
+           maximal_angle_per_state(i));
+    end
+end
+disp(txt1); %average backangle is ~11deg for neutral & happy, ~13 for sad and angry
+
+% exploratory step 2: decrease the backangle by 5 degrees and calculate y'
+% requirements:
+%   - Z (height) should stay the same; the trunk 'height' should be equal
+%   compared to pre-transformation
+%   - X (width) should stay the same; the width of the shoulders / hips
+%   should be equal compared to pre-transformation
+%   - Y ('length') should change; the hips should translate slightly
+%   forward, the shoulders slightly backward. 
+%   IMPORTANT NOTE: this will decrease the length of the vector pointing from
+%   the hips to the shoulders (X/Z equal, Y decreases), but that is fine
+%   for our current purposes.
+
+% First, we try to shift the shoulders and hips an equal amount
+% forward/backward and see what that looks like
+
+%subtract 7 degrees from the back angle
+backangle_post = backangle - 7;
+
+%calculate new Y --> new_y = z / tan(90 - new_angle))
+z = squeeze(backvector(:,:,3,:));
+y = squeeze(backvector(:,:,2,:));
+y_diff = y - (z ./ tan(deg2rad(90-backangle_post)));
+
+
+% step 3: translate the shoulder and hip markers -y_diff/2 and +y_diff/2,
+% respectively
+half_y_diff = y_diff/2;
+
+data_post = data;
+
+data_post(:, :, hip_R_marker, 2, :) = squeeze(data(:, :, hip_R_marker, 2, :)) + half_y_diff;
+data_post(:, :, hip_L_marker, 2, :) = squeeze(data(:, :, hip_L_marker, 2, :)) + half_y_diff;
+            
+data_post(:, :, shoulder_R_marker, 2, :) = squeeze(data(:, :, shoulder_R_marker, 2, :)) - half_y_diff;
+data_post(:, :, shoulder_L_marker, 2, :) = squeeze(data(:, :, shoulder_L_marker, 2, :)) - half_y_diff;
+
+% step 3.1: compute new backvector for verification purposes
+[backangle_post, backvector_post, hip_middle_calculated_new, shoulder_middle_calculated_new] = calculate_backangle_backvector(data_post);
+
+save('prelim.mat', 'backangle', 'backangle_post', ...
+    'data', 'data_post', 'backvector', 'backvector_post',...
+    'y_diff');
+
+if ~do_plot && do_plot2
+%For inspection purposes; plot a random participant and state in blue, with
+%the calculated markers and resulting vector in red. Also display the back
+%angle
+    if pp == -1 && state == -1
+        pp = randi(n_participants);
+        state = randi(n_states);
+    elseif xor(pp == -1, state == -1)
+        error("Error while trying to plot:\nParticipant:\t%d\nState:\t\t\t%d",pp, state);
+    end
+
+    plot_inspection(squeeze(data(pp, state, :, :, :)), ...
+        squeeze(hip_middle_calculated_new(pp, state, :, :)), ...
+        squeeze(shoulder_middle_calculated_new(pp, state, :, :)), ...
+        squeeze(backangle_post(pp, state, :)));
+
+elseif do_plot && do_plot2
+    pp = randi(n_participants);
+    state = randi(n_states);
+    
+    a(:,:,:,:,:,1) = data;
+    a(:,:,:,:,:,2) = data_post;
+    
+    hip(:,:,:,:,1) = hip_middle_calculated;
+    hip(:,:,:,:,2) = hip_middle_calculated_new;
+    
+    shoulder(:,:,:,:,1) = shoulder_middle_calculated;
+    shoulder(:,:,:,:,2) = shoulder_middle_calculated_new;
+    
+    ba(:,:,:,1) = backangle;
+    ba(:,:,:,2) = backangle_post;
+    
+    plot_inspection_double(squeeze(a(pp, state, :,:,:,:)), ...
+        squeeze(hip(pp, state, :, :, :)), ...
+        squeeze(shoulder(pp, state, :, :, :)), ...
+        squeeze(ba(pp, state, :, :)));
+    
+end
+%% EOF
+disp(padding);
+toc
+disp(padding);
+end
+
+function [backangle, backvector, hip_middle_calculated, shoulder_middle_calculated] = calculate_backangle_backvector(data)
+% CALCULATE_BACKANGLE_BACKVECTOR Calculates the vector from the middle of
+% the hip to the middle of the shoulders and its corresponding angle
+% relative to the Z-axis.
+% Does not modify input data.
+
+n_participants = size(data,1);
+n_states = size(data,2);
+
+n_dimensions = size(data,4);
+n_time = size(data,5);
+
 hip_R_marker = 3;    %marker locations come from the Gait.dbbuild.get_conf function
 hip_L_marker = 6;
 
@@ -98,39 +266,19 @@ shoulder_middle_calculated = (shoulder_R + shoulder_L) / 2;
 
 %step 3: calculate the backangle (using dotproduct)
 % -> angle = cos-1 (a dot b)/(|a||b|)
-backangle = NaN(n_participants, n_states, n_time); % in degrees!
+backangle = NaN(n_participants, n_states, n_time); % will be in degrees! 
+backvector = NaN(n_participants, n_states, n_dimensions, n_time);
 unitvector_z = [0 0 1];
 
 for i = 1:n_participants
     for j = 1:n_states
         for k = 1:n_time
-            backvector = squeeze(shoulder_middle_calculated(i, j, :, k)) - squeeze(hip_middle_calculated(i, j, :, k));
-            backangle(i, j, k) = acos(dot(backvector, unitvector_z) / norm(backvector)) * 180 / pi;
+            backvector(i, j, :, k) = squeeze(shoulder_middle_calculated(i, j, :, k)) - squeeze(hip_middle_calculated(i, j, :, k));
+            backangle(i, j, k) = acos(dot(squeeze(backvector(i, j, :, k)), unitvector_z) / norm(squeeze(backvector(i, j, :, k)))) * 180 / pi;
         end
     end
 end
 
-if do_plot
-%For inspection purposes; plot a random participant and state in blue, with
-%the calculated markers and resulting vector in red. Also display the back
-%angle
-    pp = randi(n_participants);
-    state = randi(n_states);
-
-    plot_inspection(squeeze(data(pp, state, :, :, :)), ...
-        squeeze(hip_middle_calculated(pp, state, :, :)), ...
-        squeeze(shoulder_middle_calculated(pp, state, :, :)), ...
-        squeeze(backangle(pp, state, :)));
-
-end
-
-save('prelim.mat', 'backangle', 'data');
-%% Static Translation
-% translation of the shoulder and outer hip markers to reduce the trunk
-% angle
-
-
-toc
 end
 
 function plot_inspection(data, hipMiddle, shoulderMiddle, backangle)
@@ -158,40 +306,16 @@ ylim(ylims)
 zlim(zlims)
 view(viewparams(1), viewparams(2))
 
-function SliderCallback(sliderObj, ~)
-    t = get(sliderObj, 'value');
-end
-
-slider = uicontrol(...
-    'parent', gcf,...
-    'style', 'slider',...
-    'min', 1,...
-    'max', 600,...
-    'Value',1,...
-    'units', 'normalized',...
-    'position', [0.20 0.05 0.75 0.05],...
-    'callback', @SliderCallback);
-
-while true
-    if t == 600
-        t = 1;
-    else
-        t = t + 1;
-    end
-    
-    set(slider, 'Value', t);
-
+for j = 2:1:600
     hold off
-    scatter3(data(:,1,t), data(:,2,t), data(:,3,t), 20, 'filled' , 'b');
+    scatter3(data(:,1,j), data(:,2,j), data(:,3,j), 20, 'filled' , 'b');
     hold on
-    plot3([hipMiddle(1,t) shoulderMiddle(1,t)], ...
-        [hipMiddle(2,t) shoulderMiddle(2,t)], ...
-        [hipMiddle(3,t) shoulderMiddle(3,t)], '-or')
-%     scatter3(hipMiddle(1,i), hipMiddle(2,i), hipMiddle(3,i), 'filled', 'r');
-%     scatter3(shoulderMiddle(1,i), shoulderMiddle(2,i), shoulderMiddle(3,i), 'filled', 'r');
+    plot3([hipMiddle(1,j) shoulderMiddle(1,j)], ...
+        [hipMiddle(2,j) shoulderMiddle(2,j)], ...
+        [hipMiddle(3,j) shoulderMiddle(3,j)], '-or')
 
     delete(findall(gcf,'type','annotation'))
-    annotation('textbox', dim, 'string', sprintf("Backangle: %.2f%c", backangle(t), char(176)));
+    annotation('textbox', dim, 'string', sprintf("Backangle: %.2f%c", backangle(j), char(176)));
     
     view(viewparams(1), viewparams(2))
     xlim(xlims)
@@ -202,4 +326,74 @@ while true
 end
     
 
+end
+
+function plot_inspection_double(data, hipMiddle, shoulderMiddle, backangle)
+% PLOT_INSPECTION_DOUBLE This function plots a participant before and after
+% the transformation. IMPORTANT: the parameters should have an extra
+% dimension; pre- & post- transformation
+import helpers.flatmat
+viewparams = [120 30];
+
+xlims = [min(flatmat(data(:,1,:,1))), max(flatmat(data(:,1,:,1)))];
+ylims = [min(flatmat(data(:,2,:,1))), max(flatmat(data(:,2,:,1)))];
+zlims = [min(flatmat(data(:,3,:,1))), max(flatmat(data(:,3,:,1)))];
+
+t = 1;
+
+f1 = figure(1);
+f1.Position = [170 513 560 420];
+scatter3(data(:,1,t,1), data(:,2,t,1), data(:,3,t,1), 20, 'filled' , 'b');
+hold on 
+scatter3(hipMiddle(1,1,1), hipMiddle(2,1,1), hipMiddle(3,1,1), 'filled', 'r');
+scatter3(shoulderMiddle(1,1,1), shoulderMiddle(2,1,1), shoulderMiddle(3,1,1), 'filled', 'r');
+
+dim = [0.1 0.5 0 0];
+annotation('textbox', dim, 'string', sprintf("Backangle: %.2f%c", backangle(1,1), char(176)));
+xlim(xlims)
+ylim(ylims)
+zlim(zlims)
+view(viewparams(1), viewparams(2))
+
+axis equal tight
+hold off
+
+f2 = figure(2);
+f2.Position = [940 515 560 420];
+scatter3(data(:,1,t,2),data(:,2,t,2), data(:,3,t,2), 20, 'filled' , 'b');
+hold on 
+scatter3(hipMiddle(1,1,2), hipMiddle(2,1,2), hipMiddle(3,1,2), 'filled', 'r');
+scatter3(shoulderMiddle(1,1,2), shoulderMiddle(2,1,2), shoulderMiddle(3,1,2), 'filled', 'r');
+
+dim = [0.1 0.5 0 0];
+annotation('textbox', dim, 'string', sprintf("Backangle: %.2f%c", backangle(1,2), char(176)));
+xlim(xlims)
+ylim(ylims)
+zlim(zlims)
+view(viewparams(1), viewparams(2))
+
+axis equal tight
+hold off
+
+for j = 2:1:600
+    for uu = 1:2
+        figure(uu)
+        scatter3(data(:,1,j,uu), data(:,2,j,uu), data(:,3,j,uu), 20, 'filled' , 'b');
+        hold on
+        plot3([hipMiddle(1,j,uu) shoulderMiddle(1,j,uu)], ...
+            [hipMiddle(2,j,uu) shoulderMiddle(2,j,uu)], ...
+            [hipMiddle(3,j,uu) shoulderMiddle(3,j,uu)], '-or')
+
+        delete(findall(gcf,'type','annotation'))
+        annotation('textbox', dim, 'string', sprintf("Backangle: %.2f%c", backangle(j,uu), char(176)));
+
+        view(viewparams(1), viewparams(2))
+        xlim(xlims)
+        ylim(ylims)
+        zlim(zlims)
+        axis equal tight
+        hold off
+    end
+    pause(0.001)
+end
 end
